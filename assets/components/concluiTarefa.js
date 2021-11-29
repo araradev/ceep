520884ba1bfd6050930097a174f61f12a854c0a5
araradev <lasevo@gmail.com>
2021-11-29 02:48:08 -0300

    Finalizado o projeto ceep

---
+++ assets/components/concluiTarefa.js
@@ -0,0 +1,17 @@
+const BotaoConclui = () =>{
+    const botaoConclui = document.createElement('button')
+    botaoConclui.classList.add('check-button','material-icons')
+    botaoConclui.innerText = 'done'
+    botaoConclui.addEventListener('click', concluirTarefa)
+    return botaoConclui
+}
+
+const concluirTarefa = (evento) => {
+    const botaoConclui = evento.target
+    
+    const tarefaCompleta = botaoConclui.parentElement
+    
+    tarefaCompleta.classList.toggle('done')
+    
+}
+export default BotaoConclui
---
+++ assets/components/main.js
@@ -0,0 +1,24 @@
+// IIFE = Immediately Invoked Function Expression
+import BotaoConclui from "./concluiTarefa.js";
+import BotaoDeleta from "./deletaTarefa.js";
+const criarTarefa = (evento)=>{
+    evento.preventDefault()
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector("[data-form-input]");
+    const valor = input.value;
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
+    const conteudo = `<p class="content">${valor}</p>`
+
+    tarefa.innerHTML = conteudo
+
+    tarefa.appendChild(BotaoConclui())
+    tarefa.appendChild(BotaoDeleta())
+    lista.appendChild(tarefa)
+
+    input.value = " "
+}
+
+const novaTarefa = document.querySelector("[data-form-button]");
+novaTarefa.addEventListener('click', criarTarefa)
